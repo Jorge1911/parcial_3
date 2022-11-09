93ff8309833574cc7b9a342d5c1b7112b8b288f9
Jorge1911 <pollo.19xoco@gmail.com>
2022-11-08 22:45:16 -0600

    1.1

--- 01_crud/app.js
+++ 01_crud/app.js
@@ -1,17 +1,17 @@
 const cargarPosts=async()=>{
     let url="https://jsonplaceholder.typicode.com/posts";
-    const api=await fetch(url);
+    const api=await fetch (url);
     const data=await api.json();
     console.log(data);
     tabla=document.querySelector("#lista");
     data.map(item=>{
-tabla.innerHTML+=`
-<tr>
-<th scope="col">${item.id}</th>
-<th scope="col">${item.title}</th>
-<th scope="col">${item.body}</th>
-<td><button class="btn btn-primary btn-lg"><i class="bi bi-pencil-square"></i>Agregar</button></td>
-<td><button class="btn btn-danger btn-lg"><i class="bi bi-trash"></i>Agregar</button></td>
-</tr>`;
+        tabla.innerHTML+=`
+        <tr>
+        <th scope="row">${item.id}</th>
+        <td>${item.title}</td>
+        <td>${item.body}</td>
+        <td><button type="button" class="btn btn-outline-success"> <i class="bi bi-brush m-2"></i> Editar</button></td>
+        <td><button type="button" class="btn btn-outline-info"><i class="bi bi-person-dash m-2"></i>Eliminar</button></td>
+        </tr>`;
     })
 }
